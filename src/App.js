0eb9f621371d2a0c74ff3ad53507757ba0e581ca
Xin <78652542+Him97@users.noreply.github.com>
2023-08-04 11:22:04 +0300

    V1 (#8)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 import Router from './Router';
+import Navbar from './Component/Navbar';
+import Login from './Component/Login';
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App position-relative">
+      <Navbar/>
+      <Login/>
       <Router/>
     </div>
   );
--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,7 +1,29 @@
-import { Container } from "react-bootstrap"
+import { Container, Form, FloatingLabel, Button, FormGroup } from "react-bootstrap"
 
 export default function Login() {
-    return(
-        <Container>Login</Container>
-    )
+    return (
+        <Container className="w-50 position-relative">
+            <FormGroup className="position-absolute top-50 start-50 translate-middle">
+                <FloatingLabel
+                    label="Name"
+                    className="mb-3"
+                >
+                    <Form.Control as="textarea" placeholder="Please input your name" />
+                </FloatingLabel>
+                <FloatingLabel
+                    label="Age"
+                    className="mb-3"
+                >
+                    <Form.Control as="textarea" placeholder="Please input your age" />
+                </FloatingLabel>
+                <Form.Select className="mb-3">
+                    <option>Please select your interest</option>
+                    <option value="men">Men</option>
+                    <option value="women">Women</option>
+                    <option value="both">Both</option>
+                </Form.Select>
+                <Button variant="primary">Login</Button>
+            </FormGroup>
+        </Container>
+    );
 }
